fca807eabecdbfec2f77bba28db39b5e7e399b0a
Recep Can Saraç <recepcansarac8@gmail.com>
2023-10-30 19:59:22 +0300

    Add to FastAttackSpeed Tower Agument

---
+++ Assets/Project001/Scripts/Aguments/FastAttackSpeed.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastAttackSpeed : MonoBehaviour
+{
+    public static FastAttackSpeed instance;
+    private void Awake() => instance = this;
+
+
+    public SOTowerLevel[] towers = null;
+
+    public float[] currentSpeed;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < towers.Length; i++)
+        {
+            currentSpeed[i] = towers[i].attackSpeed;
+        }
+    }
+    private void OnDisable()
+    {
+        for (int i = 0; i < towers.Length; i++)
+        {
+            towers[i].attackSpeed = currentSpeed[i];
+        }
+    }
+    private void Start()
+    {
+        for (int i = 0; i < towers.Length; i++)
+        {
+            towers[i].attackSpeed = towers[i].attackSpeed + (towers[i].attackSpeed * 20)/100;
+        }
+    }
+}
