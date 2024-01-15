4c76d615c042a698041f160b6605c20281d1e156
Recep Can Saraç <recepcansarac8@gmail.com>
2024-01-15 15:27:38 +0300

    Level Complate Panel

--- Assets/Project001/Scripts/Managments/EnemySpawnerManagment.cs
+++ Assets/Project001/Scripts/Managments/EnemySpawnerManagment.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;  // List kullanabilmek için
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -9,52 +10,85 @@ public class EnemySpawnerManagment : MonoBehaviour
     [SerializeField] int interwaveTime;
     public SpawnableEnemy Wave = new SpawnableEnemy();
 
+    private int totalEnemies; // Toplam canavar sayýsýný saklamak için
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
+
+
+    public GameObject panel;
+
     private void Start()
     {
         StartCoroutine(SpawnController());
+        panel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (AllEnemiesDead() == true)
+        {
+
+            panel.SetActive(true);
+        }
     }
     private IEnumerator SpawnController()
     {
+        CalculateTotalEnemies();
+
         for (int i = 0; i < Wave.Wavies.Length; i++)
         {
             for (int j = 0; j < Wave.Wavies[i].Enemy.Length; j++)
             {
                 for (int a = 0; a < Wave.Wavies[i].Enemy[j].enemieNumber; a++)
                 {
+                    Transform pos;
                     if (Wave.Wavies[i].Enemy[j].isFlyable)
                     {
-                        int randomNum = Random.Range(0,2);
-                        Transform pos;
-                        if (randomNum == 0)
-                        {
-                            pos = leftTopPos;
-                        }
-                        else
-                        {
-                            pos = rightTopPos;
-                        }
-                        Instantiate(Wave.Wavies[i].Enemy[j].enemies.EnemyPrefab, pos.position, Quaternion.identity);
-                        yield return new WaitForSeconds(Wave.Wavies[i].Enemy[j].spawnTimer);
+                        int randomNum = Random.Range(0, 2);
+                        pos = (randomNum == 0) ? leftTopPos : rightTopPos;
                     }
-                    if (Wave.Wavies[i].Enemy[j].isFlyable == false)
+                    else
                     {
                         int randomNum = Random.Range(0, 2);
-                        Transform pos;
-                        if (randomNum == 0)
-                        {
-                            pos = leftPos;
-                        }
-                        else
-                        {
-                            pos = rightPos;
-                        }
-                        Instantiate(Wave.Wavies[i].Enemy[j].enemies.EnemyPrefab, pos.position, Quaternion.identity);
-                        yield return new WaitForSeconds(Wave.Wavies[i].Enemy[j].spawnTimer);
+                        pos = (randomNum == 0) ? leftPos : rightPos;
                     }
+
+                    GameObject spawnedEnemy = Instantiate(Wave.Wavies[i].Enemy[j].enemies.EnemyPrefab, pos.position, Quaternion.identity);
+                    spawnedEnemies.Add(spawnedEnemy);
+
+                    yield return new WaitForSeconds(Wave.Wavies[i].Enemy[j].spawnTimer);
                 }
             }
+
+            yield return new WaitUntil(() => AllEnemiesDead());
             yield return new WaitForSeconds(Wave.Wavies[i].WaveTimer);
         }
+
+        Debug.Log("Bölüm Tamamlandý!");
+    }
+
+    private void CalculateTotalEnemies()
+    {
+        totalEnemies = 0;
+        foreach (var wave in Wave.Wavies)
+        {
+            foreach (var enemyType in wave.Enemy)
+            {
+                totalEnemies += enemyType.enemieNumber;
+            }
+        }
+    }
+
+    private bool AllEnemiesDead()
+    {
+        for (int i = spawnedEnemies.Count - 1; i >= 0; i--)
+        {
+            if (spawnedEnemies[i] == null)
+            {
+                spawnedEnemies.RemoveAt(i);
+            }
+        }
+
+        return spawnedEnemies.Count == 0;
     }
 }
 
